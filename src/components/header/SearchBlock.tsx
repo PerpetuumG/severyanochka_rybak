115e0d063ae72851e6250b3444605ad0ba2acdcb
PerpetuumG <perpetuumg@gmail.com>
2025-08-02 18:40:52 +0300

    SearchBlock, ButtonSearch, InputBlock

--- src/components/header/SearchBlock.tsx
+++ src/components/header/SearchBlock.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react';
+import ButtonSearch from '@/components/header/ButtonSearch';
+import InputBlock from '@/components/header/InputBlock';
 
 const SearchBlock: FC = () => {
-  return <div>Search</div>;
+  return (
+    <div className={'flex flex-row gap-4 flex-grow'}>
+      <ButtonSearch />
+      <InputBlock />
+    </div>
+  );
 };
 
 export default SearchBlock;
