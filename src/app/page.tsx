8e37f82cb5671f6afa14a2b85fcb66c17bc3997a
PerpetuumG <perpetuumg@gmail.com>
2025-08-02 22:13:45 +0300

    Slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Slider from '@/components/Slider/Slider';
+
 export default function Home() {
-  return <h1>1</h1>;
+  return (
+    <main className={'w-full mx-auto mb-20'}>
+      <Slider />
+    </main>
+  );
 }
