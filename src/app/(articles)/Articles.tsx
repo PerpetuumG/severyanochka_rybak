05754363225afe261d6137549c6c55991c9ac1fd
PerpetuumG <perpetuumg@gmail.com>
2025-08-09 13:25:03 +0300

    refactor (articles)

--- src/app/(articles)/Articles.tsx
+++ src/app/(articles)/Articles.tsx
@@ -1,44 +1,22 @@
 import React from 'react';
-import { Article } from '@/types/articles';
-import ViewAllButton from '@/components/ViewAllButton';
-import ArticleCard from '@/app/(articles)/ArticleCard';
+import fetchArticles from '@/app/(articles)/fetchArticles';
+import ArticlesSection from '@/app/(articles)/ArticlesSection';
 
 const Articles = async () => {
-  let articles: Article[] = [];
-  let error = null;
-
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}/api/articles`);
-    articles = await res.json();
+    const articles = await fetchArticles();
+
+    return (
+      <ArticlesSection
+        title={'Статьи'}
+        viewAllButton={{ text: 'Все статьи', href: 'articles' }}
+        articles={articles}
+        compact
+      />
+    );
   } catch (e) {
-    console.error('Ошибка при загрузке статей в компоненте Articles: ', e);
-    error = 'Ошибка получения статей';
+    return <div className={'text-red-500'}>Ошибка: не удалось загрузить статьи</div>;
   }
-
-  if (error) {
-    return <div className={'text-red-500'}>Ошибка: {error}</div>;
-  }
-
-  return (
-    <section>
-      <div className='flex flex-col justify-center xl:max-w-[1208px] text-[#414141]'>
-        <div className='mb-4 md:mb-8 xl:mb-10 flex flex-row justify-between'>
-          <h2 className='text-2xl xl:text-4xl text-left font-bold'>Статьи</h2>
-
-          <ViewAllButton btnText={'Все статьи'} href={'articles'} />
-        </div>
-
-        {/* Список статей */}
-        <ul className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-6'>
-          {articles.map((article) => (
-            <li key={article._id} className='h-75 md:h-105'>
-              <ArticleCard {...article} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </section>
-  );
 };
 
 export default Articles;
--- src/app/(articles)/articles/page.tsx
+++ src/app/(articles)/articles/page.tsx
@@ -1,44 +1,21 @@
 import React from 'react';
-import { Article } from '@/types/articles';
-import ViewAllButton from '@/components/ViewAllButton';
-import ArticleCard from '@/app/(articles)/ArticleCard';
+import fetchArticles from '@/app/(articles)/fetchArticles';
+import ArticlesSection from '@/app/(articles)/ArticlesSection';
 
 const AllArticles = async () => {
-  let articles: Article[] = [];
-  let error = null;
-
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL!}/api/articles`);
-    articles = await res.json();
+    const articles = await fetchArticles();
+
+    return (
+      <ArticlesSection
+        title={'Все статьи'}
+        viewAllButton={{ text: 'На главную', href: '/' }}
+        articles={articles}
+      />
+    );
   } catch (e) {
-    console.error('Ошибка при загрузке статей в компоненте AllArticles: ', e);
-    error = 'Ошибка получения статей';
+    return <div className={'text-red-500'}>Ошибка: не удалось загрузить статьи</div>;
   }
-
-  if (error) {
-    return <div className={'text-red-500'}>Ошибка: {error}</div>;
-  }
-
-  return (
-    <section>
-      <div className='px-[max(12px,calc((100%-1208px)/2))] flex flex-col text-[#414141] mt-20'>
-        <div className='mb-4 md:mb-8 xl:mb-10 flex flex-row justify-between'>
-          <h2 className='text-2xl xl:text-4xl text-left font-bold'>Статьи</h2>
-
-          <ViewAllButton btnText={'Главная'} href={'/'} />
-        </div>
-
-        {/* Список статей */}
-        <ul className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-6'>
-          {articles.map((article) => (
-            <li key={article._id} className='h-75 md:h-105'>
-              <ArticleCard {...article} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </section>
-  );
 };
 
 export default AllArticles;
